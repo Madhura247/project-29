9bac45366ff220af824272c1e469cdf1b5439b43
Madhura Yuvaraj Badhe <75791284+Madhura247@users.noreply.github.com>
2021-01-17 22:36:33 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,13 +2,22 @@ const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
+var polygon, polygon_img;
+
+function preload() {
+    polygon_img = loadImage("polygon.png")
+}
 
 function setup() {
     createCanvas(1200,500);
+
     engine = Engine.create();
     world = engine.world;
-    
-    ground = new Ground(width/2, 480, width, 20)
+
+    ground = new Ground(600, 480, 1200, 20);
+
+    polygon = Bodies.circle(50, 200, 20);
+    World.add(world,polygon);
 
 
 
@@ -18,5 +27,8 @@ function setup() {
 
 function draw() {
 
+    background("rgb(58, 45, 35)")
     ground.display();
+
+    image(polygon_img, polygon.position.x, polygon.position.y, 40, 40);
 }
