d3d385de2d20b97e8d353808efee09f5bc434636
Madhura Yuvaraj Badhe <75791284+Madhura247@users.noreply.github.com>
2021-01-19 20:01:15 +0530

    Update Slingshot.js

--- Slingshot.js
+++ Slingshot.js
@@ -6,10 +6,7 @@ class SlingShot{
             stiffness: 0.04,
             length: 10
         }
-        this.pointB = pointB
-        this.sling1= loadImage("sprites/sling1.png");
-        this.sling2= loadImage("sprites/sling2.png");
-        this.sling3= loadImage("sprites/sling3.png");
+        this.pointB = pointB;
         this.sling = Constraint.create(options);
         
         World.add(world, this.sling);
@@ -20,9 +17,6 @@ class SlingShot{
     }
 
     display(){
-
-        image(this.sling1,200, 20);
-        image(this.sling2, 175, 20);
 
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
@@ -36,7 +30,7 @@ class SlingShot{
           
             line(pointA.x - 25, pointA.y, pointB.x, pointB.y);
             line(pointA.x - 25, pointA.y, pointB.x + 30, pointB.y);
-            image(this.sling3, pointA.x - 30, pointA.y - 5, 15, 30);
+            
 
             }
 
@@ -46,7 +40,7 @@ class SlingShot{
                 
             line(pointA.x + 25, pointA.y, pointB.x, pointB.y);
             line(pointA.x + 25, pointA.y, pointB.x + 30, pointB.y);
-            image(this.sling3, pointA.x + 25, pointA.y - 5, 15, 30);
+            
 
             }
 
